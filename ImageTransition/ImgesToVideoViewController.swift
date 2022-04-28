fd58e8cb6ba9d0361d95bd431804c38390401a91
NewajShovo <leoshovo@gmail.com>
2022-04-28 11:05:29 +0600

    video transition working

---
+++ ImageTransition/ImgesToVideoViewController.swift
@@ -0,0 +1,148 @@
+//
+//  ImgesToVideoViewController.swift
+//  ImageTransition
+//
+//  Created by BCL Device7 on 27/4/22.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+import MTTransitions
+
+class ImgesToVideoViewController: UIViewController {
+
+    @IBOutlet var videoView: UIView!
+    private var player: AVPlayer!
+    private var playerLayer: AVPlayerLayer!
+    private var exportButton : UIBarButtonItem!
+    var movieMaker:MTMovieMaker?
+    var fileUrl:URL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Image To Video"
+        
+        exportButton = UIBarButtonItem(title: "Export", style: .plain, target: self, action: #selector(exportButtonPressed))
+        self.navigationItem.rightBarButtonItem = exportButton
+        setUpSubviews()
+        createVideo()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer.frame = videoView.bounds
+    }
+    
+    func setUpSubviews() -> Void {
+        player = AVPlayer()
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resizeAspect
+        
+        videoView.layer.addSublayer(playerLayer)
+        
+    }
+    
+    func createVideo() -> Void {
+        let allImages = loadImages()
+        
+        let effects: [MTTransition.Effect] = [
+            .circleOpen, .circleCrop, .none,
+            .crossZoom, .dreamy, .rotateScaleFade,
+            .wipeDown, .wipeUp]
+        
+        let path = NSTemporaryDirectory().appending("CreateVideoFromImages.mp4")
+        let url = URL(fileURLWithPath: path)
+        
+        movieMaker = MTMovieMaker(outputURL: url)
+        do {
+            try movieMaker?.createVideo(with: allImages, effects: effects, frameDuration: 2.5, transitionDuration: 1, audioURL: nil, completion: {[weak self] result in
+                guard let self = self else {return}
+                switch result {
+                case .success(let url):
+                    self.fileUrl = url
+                    let playerItem = AVPlayerItem(url: url)
+                    self.player.replaceCurrentItem(with: playerItem)
+                    self.player.play()
+                    break
+                    
+                case .failure(let error):
+                    break
+                }
+            })
+            
+            /*try movieMaker?.createVideo(with: allImages, effects: effects, completion: {[weak self] result in
+                guard let self = self else {return}
+                switch result {
+                case .success(let url):
+                    self.fileUrl = url
+                    let playerItem = AVPlayerItem(url: url)
+                    self.player.replaceCurrentItem(with: playerItem)
+                    self.player.play()
+                    break
+                    
+                case .failure(let error):
+                    break
+                }
+            })*/
+        } catch  {
+            
+        }
+        
+        
+    }
+    
+    func loadImages() -> [UIImage] {
+        var images = [UIImage]()
+        for i in 1...9 {
+            if let url = Bundle.main.url(forResource: String(i), withExtension: "jpg") {
+                if let image = UIImage(contentsOfFile: url.path) {
+                    images.append(image)
+                }
+            }
+        }
+        return images
+    }
+    
+    @objc func exportButtonPressed() -> Void {
+        guard let fileUrl = fileUrl else {
+            return
+        }
+        PHPhotoLibrary.requestAuthorization{ auth in
+            switch auth {
+            case .authorized:
+                PHPhotoLibrary.shared().performChanges {
+                    let options = PHAssetResourceCreationOptions()
+                    options.shouldMoveFile = true
+                    let request = PHAssetCreationRequest.forAsset()
+                    request.addResource(with: .video, fileURL: fileUrl, options: options)
+                } completionHandler: { success, error in
+                    DispatchQueue.main.async {
+                        if success {
+                            let alert = UIAlertController(title: "Video Saved To Camera Roll", message: nil, preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { _ in
+                                
+                            }))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                }
+
+                break;
+            default:
+                print("PhotoLibrary not authorized")
+
+                break;
+            }
+        }
+        
+        
+        
+    }
+    @IBAction func restartButtonPressed(_ sender: Any) {
+        self.player.seek(to: .zero)
+        self.player.play()
+    }
+    
+
+}
